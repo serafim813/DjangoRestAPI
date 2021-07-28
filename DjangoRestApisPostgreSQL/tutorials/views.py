66e32b64465514254c8ae7f8262ea9c1c72f4b70
serafim813 <56997649+serafim813@users.noreply.github.com>
2021-07-28 17:15:34 +0300

    Update views.py

--- DjangoRestApisPostgreSQL/tutorials/views.py
+++ DjangoRestApisPostgreSQL/tutorials/views.py
@@ -7,7 +7,7 @@ import ast
 import re
 
 dsn = {
-    'dbname': 'movies_database',
+    'dbname': 'test',
     'user': config.user,
     'password': config.password,
     'host': config.host,
@@ -22,6 +22,28 @@ def tutorial_detail(request, pk):
         with psycopg2.connect(**dsn) as conn, conn.cursor() as cursor:
             k = (str(request).split("'")[1]).split('/')[-1]
             k = 'select * from test.user_get(' + k +')'
+            cursor.execute(k)
+            result = str(cursor.fetchone())
+            result = result[1:-2]
+            result = ast.literal_eval(result)
+        return JsonResponse(result)
+    result = re.match(r'^/api/v1/user/(?P<pk>[0-9]+)$', str(request).split("'")[1])
+    if (request.method == 'PUT') and (len(str(result)) != 4):
+        with psycopg2.connect(**dsn) as conn, conn.cursor() as cursor:
+            k = (str(request).split("'")[1]).split('/')[-1]
+            tutorial_data = JSONParser().parse(request)
+            tutorial_data = str(tutorial_data).replace("'", '"')
+            k = 'select * from test.user_upd(' + k + ",'" + str(tutorial_data) + "')"
+            cursor.execute(k)
+            result = str(cursor.fetchone())
+            result = result[1:-2]
+            result = ast.literal_eval(result)
+        return JsonResponse(result)
+    result = re.match(r'^/api/v1/user/(?P<pk>[0-9]+)$', str(request).split("'")[1])
+    if (request.method == 'DELETE') and (len(str(result)) != 4):
+        with psycopg2.connect(**dsn) as conn, conn.cursor() as cursor:
+            k = (str(request).split("'")[1]).split('/')[-1]
+            k = 'select * from test.user_del(' + k +')'
             cursor.execute(k)
             result = str(cursor.fetchone())
             result = result[1:-2]
@@ -109,14 +131,27 @@ def tutorial_detail(request, pk):
         return JsonResponse(result)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def tutorial_list(request):
     result = re.match(r'^/api/v1/user/comment/$', str(request).split("'")[1])
     if (request.method == 'GET') and (len(str(result)) != 4):
         with psycopg2.connect(**dsn) as conn, conn.cursor() as cursor:
-            k = 'select * from test.comment_get(0,0)'
+            k = 'select * from test.user_comment_get(0,0)'
             cursor.execute(k)
             result = str(cursor.fetchone())
             result = result[1:-2]
             result = ast.literal_eval(result)
+        return JsonResponse(result, safe=False)
+    result = re.match(r'^/api/v1/user/$', str(request).split("'")[1])
+    print(request.method)
+    if (request.method == 'POST') and (len(str(result)) != 4):
+        with psycopg2.connect(**dsn) as conn, conn.cursor() as cursor:
+            tutorial_data = JSONParser().parse(request)
+            tutorial_data = str(tutorial_data).replace("'",'"')
+            k = 'select * from test.user_ins('+"'" + str(tutorial_data) + "')"
+            cursor.execute(k)
+            result = str(cursor.fetchone())
+            result = result[1:-2]
+            result = ast.literal_eval(result)
+            print(result)
         return JsonResponse(result, safe=False)
