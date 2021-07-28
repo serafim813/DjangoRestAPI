d1e51cba4f3eb4f1bfc24c57fa399e65e5bfd224
serafim813 <56997649+serafim813@users.noreply.github.com>
2021-07-28 18:59:21 +0300

    Update settings.py

--- DjangoRestApisPostgreSQL/DjangoRestApisPostgreSQL/settings.py
+++ DjangoRestApisPostgreSQL/DjangoRestApisPostgreSQL/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'DjangoRestApisPostgreSQL.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -73,7 +73,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
